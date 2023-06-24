a190447fd9767d50020c9e169d34289e3853c9e3
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-06-24 06:53:23 +0200

    gcs initial

---
+++ ProductivityTools.Journal.ImageProvider/Function.cs
@@ -0,0 +1,24 @@
+using CloudNative.CloudEvents;
+using Google.Cloud.Functions.Framework;
+using Google.Events.Protobuf.Cloud.Storage.V1;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.Journal.ImageProvider
+{
+    public class Function : IHttpFunction
+    {
+        /// <summary>
+        /// Logic for your function goes here.
+        /// </summary>
+        /// <param name="context">The HTTP context, containing the request and the response.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public async Task HandleAsync(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello, Functions Framework.");
+        }
+    }
+
+}
