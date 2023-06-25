49f89acc92b1f3f40eecf61aad83360217fb2b84
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-06-25 17:24:18 +0200

    return image

--- ProductivityTools.Journal.ImageProvider/Function.cs
+++ ProductivityTools.Journal.ImageProvider/Function.cs
@@ -1,7 +1,9 @@
 using Google.Cloud.Functions.Framework;
 using Google.Cloud.Storage.V1;
+using Google.Events.Protobuf.Cloud.Datastream.V1;
 using Microsoft.AspNetCore.Http;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace ProductivityTools.Journal.ImageProvider
@@ -19,9 +21,20 @@ namespace ProductivityTools.Journal.ImageProvider
 
             foreach (var obj in client.ListObjects(bucketName))
             {
-                await context.Response.WriteAsync(obj.Name);
+                //await context.Response.WriteAsync(obj.Name);
                 Console.WriteLine(obj.Name);
             }
+
+            // Download file
+            using (var stream = File.OpenWrite("Untitled.png"))
+            {
+                client.DownloadObject(bucketName, "Untitled.png", stream);
+              
+            }
+            var fileInfo = new System.IO.FileInfo("Untitled.png");
+            var fileInfo2 = new Microsoft.Extensions.FileProviders.Physical.PhysicalFileInfo(fileInfo);
+            await context.Response.SendFileAsync(fileInfo2);
+
 
             await context.Response.WriteAsync("Hello World!");
         }
