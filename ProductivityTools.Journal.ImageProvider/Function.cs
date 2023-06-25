5c27b8b2e9b26761ecee56298ce71a7049fc2753
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-06-25 15:12:37 +0200

    list buckets

--- ProductivityTools.Journal.ImageProvider/Function.cs
+++ ProductivityTools.Journal.ImageProvider/Function.cs
@@ -1,13 +1,28 @@
 using Google.Cloud.Functions.Framework;
+using Google.Cloud.Storage.V1;
 using Microsoft.AspNetCore.Http;
+using System;
 using System.Threading.Tasks;
 
 namespace ProductivityTools.Journal.ImageProvider
 {
     public class Function : IHttpFunction
     {
+        const string ProjectId = "ptjournal-b53b0";
         public async Task HandleAsync(HttpContext context)
         {
+            var client = StorageClient.Create();
+
+            // Create a bucket with a globally unique name
+            var bucketName = "ptjournaltest1";
+           // var bucket = client.CreateBucket(ProjectId, bucketName);
+
+            foreach (var obj in client.ListObjects(bucketName))
+            {
+                await context.Response.WriteAsync(obj.Name);
+                Console.WriteLine(obj.Name);
+            }
+
             await context.Response.WriteAsync("Hello World!");
         }
     }
